68cde2451fb91e49e7fecf0bae471b625a21302e
theohrogalski <151564012+theohrogalski@users.noreply.github.com>
2023-11-29 16:13:19 -0500

    Create Fashion.py

---
+++ Fashion.py
@@ -0,0 +1,26 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images.shape
+
+len(train_labels)
+
+len(test_labels)
+
+plt.figure()
+plt.imshow(train_images[3])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
